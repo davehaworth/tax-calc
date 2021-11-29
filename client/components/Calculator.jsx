21bda6608f0d6c745c521198e50a830c6ad24828
Dave-Haworth <david.haworth@xtra.co.nz>
2021-11-29 17:41:36 +1300

    add calculator component

---
+++ client/components/Calculator.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Calculator () {
+  return (
+    <>
+      <input type="text"></input>
+      <button>Submit</button>
+    </>
+  )
+}
+
+export default Calculator
