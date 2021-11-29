c5cefa7a6dd850b514ce7099f6cb0a753816f960
Dave-Haworth <david.haworth@xtra.co.nz>
2021-11-29 20:18:28 +1300

    update form and event handlers

--- client/components/Calculator.jsx
+++ client/components/Calculator.jsx
@@ -1,10 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
+
+import { calculate } from '../taxhelper'
 
 function Calculator () {
+  const [salary, setSalary] = useState(0)
+
+  const [taxed, setTaxed] = useState({
+    tax: undefined,
+    newSalary: undefined
+  })
+
+  function handleChange (e) {
+    setSalary(e.target.value)
+  }
+
+  function handleSubmit () {
+    setTaxed((calculate(salary)))
+  }
+
   return (
     <>
-      <input type="text"></input>
-      <button>Submit</button>
+      <h3>Enter your salary:</h3>
+      <input name='salary' value={salary} onChange={handleChange}></input>
+      <button onClick={handleSubmit}>Submit</button>
+      <p>You will pay ${taxed.tax} in taxes and your new taxed salary is ${taxed.newSalary}</p>
     </>
   )
 }
