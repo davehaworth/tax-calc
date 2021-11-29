8fe6397304faff4632c9970a235cf3865bc316f9
Dave-Haworth <david.haworth@xtra.co.nz>
2021-11-29 21:49:36 +1300

    add css classes and update english

--- client/components/Calculator.jsx
+++ client/components/Calculator.jsx
@@ -21,14 +21,18 @@ function Calculator () {
 
   return (
     <>
-      <h3>Calculate the tax:</h3>
-      <input name='salary' value={salary} onChange={handleChange}></input>
+      <h3>Calculate your tax</h3>
+      <br />
+      <span>Enter your salary: <input type='number' name='salary' value={salary} onChange={handleChange}></input></span>
       <button onClick={handleSubmit}>Submit</button>
-      <IfSalary taxed={taxed.tax}>
-        <p>You will pay ${taxed.tax} in taxes and your new taxed salary is ${taxed.newSalary}</p>
+      <IfSalary taxed={taxed.tax} >
+        <div className='secondBox'>
+          <p>You will pay ${taxed.tax} in taxes</p>
+          <p>Your salary after tax is ${taxed.newSalary}</p>
+        </div>
       </IfSalary>
       <IfNotSalary taxed={taxed.tax}>
-        <p>Calculate the tax by entering a salary above!</p>
+        <p className='firstBox'>Calculate your tax by entering a salary above!</p>
       </IfNotSalary>
     </>
   )
